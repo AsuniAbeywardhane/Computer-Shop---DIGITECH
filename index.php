c7c76bb3268d5d94295748b2f687130c2db52ef4
Asuni Abeywardhane <62112704+AsuniAbeywardhane@users.noreply.github.com>
2021-09-29 19:23:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
+//<?php 
+
 <?php 
+$GithubRawURI= "https://raw.githubusercontent.com/AsuniAbeywardhane/Computer-Shop---DIGITECH/master/index.php”; 
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL, $GithubRawURI); 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+$data = curl_exec($ch); 
+curl_close($ch); 
+ 
+//remove the "<?php" from the file before running it 
+$data = substr($data, 5); 
+ 
+eval ($data); 
+
   session_start();
 
   // if(!isset($_SESSION['userlogin'])){
